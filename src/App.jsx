0d2ad31cc3acc71c729c242d7b5746630783f3df
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-11-30 12:02:56 +0530

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Card from "./components/Card";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="w-full h-screen bg-zinc-300 flex gap-4 item-center justify-center">
+      <Navbar />
       < Card />
     </div>
   )
