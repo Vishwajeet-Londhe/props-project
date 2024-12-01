904bef07c9cb3ef55465b5b29f4d82bc41407f11
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-02 00:12:27 +0530

    flex and gap done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Card() {
   return (
-    <div className='w-60 bg-zinc-100 p-4 rounded-md'>
+    <div className='w-60 bg-zinc-100 p-4 rounded-md flex gap-4'>
       <div className='w-20 h-20 bg-orange-600 rounded-md'></div>
       <div className=''>
         <h3 className='text-xl font-semibold'>Song Name</h3>
