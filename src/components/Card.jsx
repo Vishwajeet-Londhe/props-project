f72b8d77809818754e5622d5bbc4cd167f346aa4
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-02 00:03:01 +0530

    Card block, image, text

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Card() {
   return (
-    <div>
-      Card
+    <div className='w-60 bg-zinc-100 p-4 rounded-md flex gap-4'>
+      <div className='w-20 h-20 bg-orange-600 rounded-md'></div>
+      <div className=''></div>
     </div>
   )
 }
