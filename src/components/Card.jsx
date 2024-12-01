73cc74f14e1f980c5bf99796b19495171cddaa3a
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-02 00:07:30 +0530

    added song and artist name

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,10 @@ function Card() {
   return (
     <div className='w-60 bg-zinc-100 p-4 rounded-md flex gap-4'>
       <div className='w-20 h-20 bg-orange-600 rounded-md'></div>
-      <div className=''></div>
+      <div className=''>
+        <h3 className='text-xl font-semibold'>Song Name</h3>
+        <h6 className='text-sm'>Artist Name</h6>
+      </div>
     </div>
   )
 }
