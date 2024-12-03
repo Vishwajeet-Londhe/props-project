fa19d220c748e45131efa7610f4bcd116cbdb066
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-03 22:31:48 +0530

    gives card flex wrap

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="w-full h-screen bg-zinc-300">
       <Navbar />
-      <div className="px-20 flex gap-10">
+      <div className="px-20 flex gap-10 flex-wrap">
         < Card />
         < Card />
         < Card />
