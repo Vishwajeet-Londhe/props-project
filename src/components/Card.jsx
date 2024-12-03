e58e0d112a3314ce850898cff6e11edfbcfdb510
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-12-03 23:13:51 +0530

    image,songname,artistname shows in card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,10 +6,12 @@ function Card({data}) {
 
   return (
     <div className='w-60 bg-zinc-100 p-4 rounded-md flex gap-4 pb-10 relative mt-10'>
-      <div className='w-20 h-20 bg-orange-600 rounded-md'></div>
+      <div className='w-20 h-20 bg-orange-600 rounded-md overflow-hidden'>
+        <img className='w-full h-full object-cover' src={image} alt="" />
+      </div>
       <div className=''>
-        <h3 className='text-xl font-semibold'>Song Name</h3>
-        <h6 className='text-sm'>Artist Name</h6>
+        <h3 className='text-xl font-semibold'>{SongName}</h3>
+        <h6 className='text-sm'>{ArtistName}</h6>
       </div>
       <button className='px-4 py-3 bg-orange-600 absolute bottom-0 whitespace-nowrap left-1/2 -translate-x-[50%] translate-y-[50%] text-white text-xs rounded-full'>Add to Favourite</button>
     </div>
